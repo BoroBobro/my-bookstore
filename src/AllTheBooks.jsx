ef001350c30fcaa7a06846cad04a04950ac29fc5
Borislav Kozhuharski <nomercyforyouss@gmail.com>
2025-05-14 15:34:56 +0200

    Single Book e BookList creati come prova

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,9 +1,10 @@
 
 import { Card, Container, Row, Col } from 'react-bootstrap'
 import fantasyBooks from './data/fantasy.json'
+import historyBooks from './data/history.json'
 
 const AllTheBooks = () => (
-  <Container className="my-5">
+  <Container fluid className="my-5">
     <Row>
       {fantasyBooks.map((book) => (
         <Col md={4} lg={3} key={book.asin} className="mb-4">
@@ -16,6 +17,21 @@ const AllTheBooks = () => (
           </Card>
         </Col>
       ))}
+    </Row>
+    <Row>
+        {
+            historyBooks.map((book) =>(
+                <Col md={4} lg={3} key={book.asin} className='mb-4'>
+                    <Card>
+                        <Card.Img variant='top' src={book.img} alt={book.title}/>
+                        <Card.Body>
+                            <Card.Title>{book.title}</Card.Title>
+                            <Card.Title>{book.price}</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))
+        }
     </Row>
   </Container>
 )
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,5 @@ import 'bootstrap/dist/css/bootstrap.min.css' // Import Bootstrap
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 )
